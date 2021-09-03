dc85352bf120ad9008ef9c14fc715bee72bb95e8
mazellat <thomas.mazella@laposte.net>
2021-09-03 08:41:56 +0200

    Adding software project

---
+++ Software/emseq/Core/Src/display.c
@@ -0,0 +1,38 @@
+/**
+  ******************************************************************************
+  * @file           : display.c
+  * @author			: Thomas Mazella - Electronic Milkshake
+  * @date			: Sept 2021
+  * @brief          : Display and LEDs related operations
+  ******************************************************************************
+  */
+
+/* Includes ------------------------------------------------------------------*/
+#include "display.h"
+#include "sequencer.h"
+
+/* Typedef -----------------------------------------------------------*/
+
+/* Defines ------------------------------------------------------------*/
+#define MAX_SEQ_LEN 16
+
+/* Macros -------------------------------------------------------------*/
+
+/* Variables ---------------------------------------------------------*/
+
+/* Private function -----------------------------------------------*/
+
+/* Public function -----------------------------------------------*/
+
+/**
+ * @fn static void Display_vUpdateInterface(void)
+ * @brief This function refreshes every visible element of the module (called on timer callback)
+ */
+PUBLIC static void Display_vUpdateInterface(void)
+{
+	/* Update matrix */
+
+	/* Update LEDs */
+
+}
+
